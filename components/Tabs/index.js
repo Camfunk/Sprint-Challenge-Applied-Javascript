036314f4cf63df39dffdb4ac58322fc0ed78804e
camfunk <cameronfunk92@gmail.com>
2019-12-06 12:50:26 -0500

    finished cards, mvp met

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -4,6 +4,7 @@
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
+
 const topicsDiv = document.querySelector(".topics");
 axios
   .get("https://lambda-times-backend.herokuapp.com/topics")
