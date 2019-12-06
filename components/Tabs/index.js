62602fd41ee09ba49a4a86c60ff58df4ff1db2f9
camfunk <cameronfunk92@gmail.com>
2019-12-06 11:20:47 -0500

    axios request logging properly

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -4,6 +4,13 @@
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
+const topicsDiv = document.querySelector(".topics");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+  });
+
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
